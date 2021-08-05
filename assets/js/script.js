656565454437b89aa04ba1fd9e121757e91e911d
Abby Roberg <abbyroberg@gmail.com>
2021-08-04 20:24:42 -0500

    js link

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// function to get a random state capital city when search button clicked
+var getCity = function() {
+
+}
+
+// function to get highest rated restaurant in given city from Yelp
+var getRestaurant = function(city) {
+    
+}
