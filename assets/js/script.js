fc50f15bc0fc7d34d1605ee425fa4ef8dafd4c5d
Abby Roberg <abbyroberg@gmail.com>
2021-08-11 23:56:31 -0500

    added nutrition facts to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,11 +45,9 @@ var getRecipe = function(event) {
 
             // get an array list of the recipe ingredients
             ingredients = data.hits[recipeNum].recipe.ingredientLines;
-            console.log(ingredients);
 
-            //Somehow append array to recipeList / Researching
+            // append each ingredient as an li
             for(var i=0; i<ingredients.length; i++) {
-                console.log(ingredients[i]);
                 $("#recipeList").append("<li>" + ingredients[i] + "</li>");
             }
             
@@ -58,19 +56,21 @@ var getRecipe = function(event) {
 
             // Add link to bottom of the box for people to click on
             $("#recipeLink").html('<a href="' + recipeUrl + '">Click here to view full recipe</a>')
-        .then(getNutrition);
+
+        .then(getNutrition(recipeName));
     })
 })
 }
 
 // function to get nutrition facts for recipe
-var getNutrition = function() {
+var getNutrition = function(recipe) {
+
+    console.log("getNutrition running!" + recipe);
 
     let searchCode = "";
 
     // make ingredients search friendly
     for(var i=0; i<ingredients.length; i++) {
-        console.log(ingredients[i]);
         let ingrClean = ingredients[i].split("(",1);
         let ingrClean2 = ingrClean[0].split(",",1);
        
@@ -82,7 +82,6 @@ var getNutrition = function() {
     // get nutrition for each ingredient & add to totals
     for(var i=0; i<ingredients.length; i++) {
         searchCode = ingredients[i];
-        console.log(searchCode);
         
         // request nutrition info from API for ingredient
         let apiCall = "https://api.edamam.com/api/nutrition-data?app_id=c15e6a0d&app_key=4fab285b283f1834189d7b315b26dbd6&nutrition-type=cooking&ingr=" + searchCode;
@@ -94,18 +93,22 @@ var getNutrition = function() {
                 // add ingredient calories to total
                 calories += data.calories;
                 console.log("Calories: " + calories);
+                $('#calories-box').text('Total Calories: ' + calories);
     
                 // add ingredient fat to total
                 fat += Math.floor(data.totalNutrients.FAT.quantity);
                 console.log("Fat: " + fat + data.totalNutrients.FAT.unit);
+                $('#fat-box').text('Total Fat: ' + fat);
     
                 // add ingredient protein to total
                 protein += Math.floor(data.totalNutrients.PROCNT.quantity);
-                console.log("Protein: " + protein + data.totalNutrients.PROCNT.unit)
+                console.log("Protein: " + protein + data.totalNutrients.PROCNT.unit);
+                $('#protein-box').text('Total Protein: ' + protein);
     
                 // add ingredient carbs to total
                 carbs += Math.floor(data.totalNutrients.CHOCDF.quantity);
                 console.log("Carbs: " + carbs + data.totalNutrients.CHOCDF.unit)
+                $('#carbs-box').text('Total Carbs: ' + carbs);
 
             })
         })
